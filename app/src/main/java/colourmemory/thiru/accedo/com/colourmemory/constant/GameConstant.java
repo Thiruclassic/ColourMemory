9c750ac216ed979c1eb4a6141c0994c0eb20334c
thiruclassic <thiruclassic@gmail.com>
2018-04-04 00:22:21 +0800

    adding colour game

---
+++ app/src/main/java/colourmemory/thiru/accedo/com/colourmemory/constant/GameConstant.java
@@ -0,0 +1,15 @@
+package colourmemory.thiru.accedo.com.colourmemory.constant;
+
+/**
+ * Created by Thirumal on 29/3/2018.
+ */
+
+public class GameConstant {
+
+    public static final String SCORE = "Score";
+    public static final String SPACE = " ";
+    public static final String COLON = ":";
+    public static final String CORRECT = "Correct";
+    public static final String WRONG = "Wrong";
+
+}
